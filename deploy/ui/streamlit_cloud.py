c6a3ab780aef2a8ff9a6a113b1f05a849d0ef03c
Edwina Abam <borteleabam@gmail.com>
2026-02-18 00:01:49 +0000

    deploy forecasting model on streamlit

--- deploy/ui/streamlit_cloud.py
+++ deploy/ui/streamlit_cloud.py
@@ -34,7 +34,7 @@ xgb_fallback = pickle.load(open(
 # Sidebar Logo
 # ==========================================
 logo = Image.open(os.path.join(PROJECT_ROOT, "deploy", "ui", "logo.png"))
-st.sidebar.image(logo, width=200)
+st.sidebar.image(logo, use_container_width=True)
 st.sidebar.markdown("## AlloyTower Real Estate")
 
 # ==========================================
